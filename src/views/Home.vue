5aedc1cfd4c231da3a3caea845e76a829d1bf39f
miracleonyenma <miracleficient@gmail.com>
2020-07-11 16:05:23 +0100

    dep: changes made from vue-router installation

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <appHeader></appHeader>
+    <main>
+      <!-- <img alt="Vue logo" src="../assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    </main>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import appHeader from '@/components/app-header.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    appHeader,
+    // HelloWorld
+  }
+}
+</script>
