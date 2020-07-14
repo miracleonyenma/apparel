c6e821dd33f72f6722471a5aaf6632ce88c92166
miracleonyenma <miracleficient@gmail.com>
2020-07-14 15:11:03 +0100

    fix: remove default styling in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="--max-width-1280">
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -14,26 +14,19 @@ export default {
   
 }
 </script>
-<style>
+<style scoped>
 
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#app{
+  padding: 0 2em;
 }
 
-#nav {
-  padding: 30px;
+@media screen and (min-width: 768px){
+    #app{
+      padding: 0 4em;
+    }
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
+/* #nav a.router-link-exact-active {
   color: #42b983;
-} */
+}  */
 </style>
